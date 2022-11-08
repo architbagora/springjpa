446e23b8e6eddedd539b0e344a7fd404c0c158c3
Archit Bagora <45651372+architbagora@users.noreply.github.com>
2022-11-08 23:32:45 +0000

    skeleton code

---
+++ springbootJPAin28minscourse/springbootJPAin28minscourse/src/main/java/com/archit/springbootJPAin28minscourse/SpringbootJpAin28minscourseApplication.java
@@ -0,0 +1,13 @@
+package com.archit.springbootJPAin28minscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpAin28minscourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpAin28minscourseApplication.class, args);
+	}
+
+}
---
+++ springbootJPAin28minscourse/springbootJPAin28minscourse/src/test/java/com/archit/springbootJPAin28minscourse/SpringbootJpAin28minscourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archit.springbootJPAin28minscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpAin28minscourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
